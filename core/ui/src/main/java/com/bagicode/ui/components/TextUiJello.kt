79624e15c8e12cf27ac9fab19a9cdfd1fa88bdeb
kickymaulana <kickymaulana@gmail.com>
2025-12-19 09:21:06 +0700

    Component text Header Lanjutan

--- core/ui/src/main/java/com/bagicode/ui/components/TextUiJello.kt
+++ core/ui/src/main/java/com/bagicode/ui/components/TextUiJello.kt
@@ -1,32 +1,46 @@
 package com.bagicode.ui.components
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Devices.NEXUS_5
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
 @Composable
-fun JelloTextHeader() {
+fun JelloTextHeader(
+    text: String = "Welcome to Login",
+    color: Color = Color.Black,
+    modifier: Modifier = Modifier.padding(16.dp)
+) {
     Text(
-        text = "Welcome to Login",
+        text = text,
+        modifier = modifier,
         style = TextStyle(
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
             color = Color.Black,
             lineHeight = 24.sp,
             textAlign = TextAlign.Left
-        )
-
+        ),
+        overflow = TextOverflow.Ellipsis,
+        color = color,
+        maxLines = 1
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = NEXUS_5, showSystemUi = true)
 @Composable
 fun JelloTextHeaderPreview() {
-    JelloTextHeader()
+    JelloTextHeader(
+        text = "Roby ganteng banget nih dan tertamvan didunia"
+    )
 }
