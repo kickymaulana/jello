f40e8955a5890c2be2639b003fe2df6af9e22235
kickymaulana <kickymaulana@gmail.com>
2025-12-19 08:45:36 +0700

    membuat textuijello

--- core/ui/src/main/java/com/bagicode/ui/components/TextUiJello.kt
+++ core/ui/src/main/java/com/bagicode/ui/components/TextUiJello.kt
@@ -1,4 +1,32 @@
 package com.bagicode.ui.components
 
-class TextUiJello {
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun JelloTextHeader() {
+    Text(
+        text = "Welcome to Login",
+        style = TextStyle(
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            lineHeight = 24.sp,
+            textAlign = TextAlign.Left
+        )
+
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun JelloTextHeaderPreview() {
+    JelloTextHeader()
 }
