3790f14095a23aa04bf61487b3d84f4989434a0f
kickymaulana <kickymaulana@gmail.com>
2025-12-19 09:32:08 +0700

    component text desk click

--- core/ui/src/main/java/com/bagicode/ui/components/TextUiJello.kt
+++ core/ui/src/main/java/com/bagicode/ui/components/TextUiJello.kt
@@ -1,14 +1,19 @@
 package com.bagicode.ui.components
 
+import android.R
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Devices.NEXUS_5
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,10 +42,39 @@ fun JelloTextHeader(
     )
 }
 
-@Preview(showBackground = true, device = NEXUS_5, showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
 fun JelloTextHeaderPreview() {
     JelloTextHeader(
         text = "Roby ganteng banget nih dan tertamvan didunia"
     )
+}
+
+@Composable
+fun JelloTextRegularWithClick(){
+    val annotatedText = buildAnnotatedString {
+        append("Please fill E-mail & password to login your app account.")
+        pushStringAnnotation(
+            tag = "TEXT_CLICK",
+            annotation = " Sign Up"
+        )
+        withStyle(
+            style = SpanStyle(
+                color = Color.Blue,
+                fontWeight = FontWeight.Bold
+            )
+        ){
+            append(" Sign Up")
+        }
+        pop()
+
+    }
+    ClickableText(text = annotatedText) { }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun JelloTextRegularWithClickPreview(){
+    JelloTextRegularWithClick()
 }
